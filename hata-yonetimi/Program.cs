2c75ef15f2aba67b5a0f149f711fe403447e724f
Mustafa Gokturk Caglar <mustafagcaglar@gmail.com>
2022-08-15 23:56:41 +0300

    change

--- hata-yonetimi/Program.cs
+++ hata-yonetimi/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// try
+// {
+//     Console.WriteLine("Bir sayı giriniz:");
+//     int sayi = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Girmiş olduğunuz sayi :" + sayi);
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine("Hata: "+ ex.Message.ToString());
+// }
+// finally
+// {
+//      Console.Write("İşlem tamamlandı.");
+// }
+
+try
+{
+    //int a = int.Parse(null);
+    //int a = int.Parse("test");
+    int a = int.Parse("-2000000000");
+}
+catch (ArgumentNullException ex)
+{
+  Console.WriteLine("Boş değer girdiniz");
+  Console.WriteLine(ex);
+}
+catch(FormatException ex)
+{
+  Console.WriteLine("Veri tipi uygun değil.");
+  Console.WriteLine(ex);
+}
+catch(OverflowException ex)
+{
+  Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz.");
+  Console.WriteLine(ex);
+}
+finally{
+    Console.WriteLine("İşlem başarıyla tamamlandı.");
+}
