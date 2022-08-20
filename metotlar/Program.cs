83c57c37acb8eb01477663742a844d3cbdc4538e
Mustafa Gokturk Caglar <mustafagcaglar@gmail.com>
2022-08-20 22:04:14 +0300

    metotlar proje eklendi

---
+++ metotlar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erisim_belirteci geri_donuctipi metod_adi(parametre listesi/arguman)
+            // {
+                ////komutlar;
+                // return;
+            // }
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+            
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+            
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc)); //string veri tipini int olarak verdiğimiz için stringe dönüştürdük 
+
+            int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));        
+            ornek.EkranaYazdir(Convert.ToString(a+b)); 
+        }
+        
+        static int Topla(int deger1, int deger2);
+        {
+            return (deger1 + deger2);
+        }
+    }
+    
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+        Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(ref int deger1, ref int deger2);
+        {
+            deger1+=1;
+            deger2+=1;
+            return deger1 + deger2;
+        }
+    }
+}    
