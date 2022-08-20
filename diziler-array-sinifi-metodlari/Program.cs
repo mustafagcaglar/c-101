27ce645fd060d63c1d8dc9ed141599fbc2dce093
Mustafa Gokturk Caglar <mustafagcaglar@gmail.com>
2022-08-20 17:53:20 +0300

    array proje eklendi

--- diziler-array-sinifi-metodlari/Program.cs
+++ diziler-array-sinifi-metodlari/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//sort
+int[] sayiDizisi ={23,12,4,86,72,3,11,17};
+
+Console.WriteLine("***** Sırasız Dizi *****");
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi); 
+
+Console.WriteLine("***** Sıralı Dizi *****");
+Array.Sort(sayiDizisi);
+
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+//clear
+Console.WriteLine("***** Array Clear *****");
+//sayı dizisi elemanlarını kullanarak 2. indexten itibaren 2 elemanı sıfırlar
+Array.Clear(sayiDizisi,2,2);
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+//Reverse
+Console.WriteLine("***** Array Reverse *****");
+Array.Reverse(sayiDizisi);
+
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+//indexof
+Console.WriteLine("***** Array Indexof *****");
+
+Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+//Resize
+Console.WriteLine("***** Array Resize *****");
+Array.Resize<int>(ref sayiDizisi,9);
+sayiDizisi[8] = 99;
+
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+
+
+
