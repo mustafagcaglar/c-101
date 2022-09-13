557a606ce8758cd23c8ccf1a2e0bffe4c1fedc74
Mustafa Gokturk Caglar <mustafagcaglar@gmail.com>
2022-09-14 01:15:05 +0300

    enum proje eklendi

---
+++ csharp-enum/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sıcaklık = 32;
+            if(sıcaklık <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim.");
+            else if (sıcaklık >= (int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            else if (sıcaklık >= (int)HavaDurumu.Normal && sıcaklık<(int)HavaDurumu.CokSıcak)
+                Console.WriteLine("Hadi Dışarıya Çıkalım.");
+
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 0,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak =30
+    }
+}
